755a8b447dfab96e5298bda5a2b0b8bbbe5304c2
gustavo-bonomo <cidadenovarp@gmail.com>
2025-10-07 08:45:40 -0300

    Add files via upload

--- testes.js
+++ testes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json()); // para receber JSON no body
 
+app.get('/retornar/:texto', (req, res) => {
+  const texto = req.params.texto;
+  res.send(`Você enviou: ${texto}`);
+});
+
 app.get('/', (req, res) => {
   res.send('Olá, mundo!');
 });
 
-app.post('/retornar/:texto', (req, res) => {
-  const texto = req.params.texto;
-  res.send(`Você enviou: ${texto}`);
-});
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
